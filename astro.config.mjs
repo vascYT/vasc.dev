a0451f61a529fae21dfd8a7d4530202cc101b140
vascYT <nicovanaschaufler@gmail.com>
2023-08-15 03:22:10 +0200

    Add @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vasc.dev/",
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "dark-plus",
